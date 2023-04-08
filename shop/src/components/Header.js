290cc205a06907da0f577ab979739f3dd169fced
Oleh Novakovskyi <oleg.novakovskiy@gmail.com>
2023-04-08 19:10:08 +0300

    Step 4
    Create a shopping cart

--- shop/src/components/Header.js
+++ shop/src/components/Header.js
@@ -1,14 +1,25 @@
+import React, {useState} from "react";
+import { FaShoppingCart } from "react-icons/fa";
+
 const Header = () => {
+
+  let [cartOpen, setCartOpen] = useState(false);
   return (
     <header>
       <div>
         <span className="logo">Blueberry Shop</span>
         <ul className="nav">
-            <li>about us</li>
-            <li>contacts</li>
-            <li>profile</li>
+            <li>About us</li>
+            <li>Contacts</li>
+            <li>Profile</li>
         </ul>
+        <FaShoppingCart onClick={() => setCartOpen(cartOpen = !cartOpen)}  className={`shop-cart-btn ${cartOpen && 'active'}`} />
       </div>
+      {cartOpen && (
+        <div className="shop-cart">
+          <h1>Cart</h1>
+        </div>
+      )}
       <div className="presentation"></div>
     </header>
   );
