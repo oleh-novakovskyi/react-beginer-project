726b051e37ed915c964bf371481f975e8095b5ab
Oleh Novakovskyi <oleg.novakovskiy@gmail.com>
2023-04-11 13:23:06 +0300

    Step 7
    add filter products

--- shop/src/App.js
+++ shop/src/App.js
@@ -2,19 +2,21 @@ import React from "react";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Products from "./components/Products";
+import Categories from "./components/Categories";
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       orders: [],
+      currentProducts: [],
       products: [
         {
           id: 1,
           title: "Лохина Блюголд (Bluegold)",
           img: "350x350.jpg",
           desc: "Ягоди лохини цього популярного сорту світло-сині, з щільною ароматною м’якоттю.Та й самі кущики пишні і красиві, тому стануть чудовою прикрасою будь-якого саду. ",
-          category: "medium",
+          category: "middle",
           price: "100",
         },
         {
@@ -22,7 +24,7 @@ class App extends React.Component {
           title: "Лохина Блюкроп",
           img: "350x350.jpg",
           desc: "Цей середньостиглий американський сорт лохини був виведений понад півстоліття тому. Ягоди світло-сині, великі з пружною м’якоттю. Їх легко можна впізнати за характерною плескатою формою.",
-          category: "medium",
+          category: "middle",
           price: "120",
         },
         {
@@ -54,7 +56,7 @@ class App extends React.Component {
           title: "Лохина Дюк",
           img: "350x350.jpg",
           desc: "Цей сорт надзвичайно популярний у себе на батьківщині в Америці. І це не дивно: рослини стійкі до морозів, стабільно дають високий урожай. Кущам Дюка не страшні весняні заморозки, тому що вони пізно зацвітають, зате п лоди дають досить рано – вже в середині липня.",
-          category: "medium",
+          category: "middle",
           price: "160",
         },
         {
@@ -70,7 +72,7 @@ class App extends React.Component {
           title: "Лохина Торо",
           img: "350x350.jpg",
           desc: "Кущ Торо має прекрасний декоративний вигляд. Ягода дозріває в кінці липня-початку серпня. Урожайність висока. Урожай збирається в 2 збори.",
-          category: "medium",
+          category: "middle",
           price: "125",
         },
         {
@@ -78,7 +80,7 @@ class App extends React.Component {
           title: "Патріот",
           img: "350x350.jpg",
           desc: "Кущі цього сорту відмінно почувають себе в ґрунті будь-якого типу, хоча найбільше люблять важкі, добре зволожені ґрунти. А ще вони чудово зимують, не бояться фітофторозу стебел.",
-          category: "medium",
+          category: "middle",
           price: "145",
         },
         {
@@ -91,18 +93,32 @@ class App extends React.Component {
         },
       ],
     };
+    this.state.currentProducts = this.state.products;
     this.addToOrder = this.addToOrder.bind(this);
     this.deleteOrder = this.deleteOrder.bind(this);
+    this.chooseCategory = this.chooseCategory.bind(this);
   }
   render() {
     return (
       <div className="wrapper">
         <Header orders={this.state.orders} onDelete={this.deleteOrder} />
-        <Products products={this.state.products} onAdd={this.addToOrder} />
+        <Categories chooseCategory={this.chooseCategory} />
+        <Products products={this.state.currentProducts} onAdd={this.addToOrder} />
         <Footer />
       </div>
     );
   }
+
+  chooseCategory(category) {
+    if(category === 'all'){
+      this.setState({currentProducts: this.state.products});
+      return
+    }
+    this.setState({
+      currentProducts: this.state.products.filter(el => el.category === category)
+    })
+  }
+
   deleteOrder(id){
     this.setState({orders: this.state.orders.filter(el => el.id !== id)})
   }
